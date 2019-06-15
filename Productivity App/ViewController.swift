4928b4f54b8747571b08e045d2cb8b560485cedc
grantb2277 <grant43140@gmail.com>
2019-06-15 10:27:30 -0500

    Initial Commit

---
+++ Productivity App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Productivity App
+//
+//  Created by Grant Brooks on 6/15/19.
+//  Copyright © 2019 Grant Brooks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
